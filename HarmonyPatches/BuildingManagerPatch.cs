26f2ce131ec9c9c9047102106796f830b44bfe2a
Tal <talrofeh@gmail.com>
2024-02-09 22:10:17 +0200

    fix bug

--- HarmonyPatches/BuildingManagerPatch.cs
+++ HarmonyPatches/BuildingManagerPatch.cs
@@ -1,44 +1,52 @@
 ﻿using ColossalFramework;
-using ColossalFramework.UI;
 using CombinedAIS.AI;
 using HarmonyLib;
-using System.Linq;
 
 namespace CombinedAIS.HarmonyPatches
 {
     [HarmonyPatch(typeof(BuildingManager))]
     internal static class BuildingManagerPatch
     {
-        [HarmonyPatch(typeof(BuildingManager), "GetServiceBuildings")]
+        [HarmonyPatch(typeof(BuildingManager), "AddServiceBuilding")]
         [HarmonyPrefix]
-        public static bool GetServiceBuildings(ItemClass.Service service, ref FastList<ushort>[] ___m_serviceBuildings, ref FastList<ushort> __result)
+        public static bool AddServiceBuilding(ushort building, ItemClass.Service service, ref FastList<ushort>[] ___m_serviceBuildings)
         {
-            if (service == ItemClass.Service.Hotel)
+            var buffer = Singleton<BuildingManager>.instance.m_buildings.m_buffer;
+            
+            if(service == ItemClass.Service.Monument || service == ItemClass.Service.Beautification || service == ItemClass.Service.PublicTransport)
             {
-                int publicServiceIndex = ItemClass.GetPublicServiceIndex(service);
-                if (publicServiceIndex != -1)
+                if (buffer[building].Info != null && (buffer[building].Info.GetAI() is HotelAI || buffer[building].Info.GetAI() is AirportHotelAI || buffer[building].Info.GetAI() is ParkHotelAI))
                 {
-                    __result = ___m_serviceBuildings[publicServiceIndex];
-                    UpdateHotelList(ref __result);
-                    return false;
+                    int publicServiceIndex = ItemClass.GetPublicServiceIndex(ItemClass.Service.Hotel);
+                    if (publicServiceIndex != -1)
+                    {
+                        ___m_serviceBuildings[publicServiceIndex].Add(building);
+                        return false;
+                    }
                 }
-                __result = null;
-                return false;
             }
             return true;
         }
 
-        private static void UpdateHotelList(ref FastList<ushort> Hotels)
+        [HarmonyPatch(typeof(BuildingManager), "RemoveServiceBuilding")]
+        [HarmonyPrefix]
+        public static bool RemoveServiceBuilding(ushort building, ItemClass.Service service, ref FastList<ushort>[] ___m_serviceBuildings)
         {
             var buffer = Singleton<BuildingManager>.instance.m_buildings.m_buffer;
-            var length = buffer.Length;
-            for (ushort buildingId = 0; buildingId < length; buildingId++)
+
+            if (service == ItemClass.Service.Monument || service == ItemClass.Service.Beautification || service == ItemClass.Service.PublicTransport)
             {
-                if (buffer[buildingId].Info != null && (buffer[buildingId].Info.GetAI() is HotelAI || buffer[buildingId].Info.GetAI() is AirportHotelAI || buffer[buildingId].Info.GetAI() is ParkHotelAI) && !Hotels.m_buffer.Contains(buildingId))
+                if (buffer[building].Info != null && (buffer[building].Info.GetAI() is HotelAI || buffer[building].Info.GetAI() is AirportHotelAI || buffer[building].Info.GetAI() is ParkHotelAI))
                 {
-                    Hotels.Add(buildingId);
+                    int publicServiceIndex = ItemClass.GetPublicServiceIndex(ItemClass.Service.Hotel);
+                    if (publicServiceIndex != -1)
+                    {
+                        ___m_serviceBuildings[publicServiceIndex].Remove(building);
+                        return false;
+                    }
                 }
             }
+            return true;
         }
     }
 }
